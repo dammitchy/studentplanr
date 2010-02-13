550ec8645788ab9fc45041588b73a93876e60fb4
Matt Gornick <mattgornick@gmail.com>
2010-02-13 15:40:27 -0600

    Matt and Jurand pairing. added allcalendars feed into OAuth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,13 @@ class HomeController < ApplicationController
   def index
     @token = params[:token]
     
-    # client = GData::Client::DocList.new
-    #     client.authsub_token = params[:token] # extract the single-use token from the URL query params
-    #     session[:token] = client.auth_handler.upgrade()
-    #     client.authsub_token = session[:token] if session[:token]
-    #     
-    #     @feed = client.get('http://docs.google.com/feeds/documents/private/full').to_xml
+    client = GData::Client::DocList.new
+    client.authsub_token = params[:token] # extract the single-use token from the URL query params
+    session[:token] = client.auth_handler.upgrade()
+    client.authsub_token = session[:token] if session[:token]
+    
+    @feed = client.get('http://www.google.com/calendar/feeds/default/allcalendars/full').to_xml
+            
   end
   
   def home    
