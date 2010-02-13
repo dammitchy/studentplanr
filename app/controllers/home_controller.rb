a6b524136016c0d3b187b83a49445a35762b79ed
Matt Gornick <mattgornick@gmail.com>
2010-02-13 15:02:00 -0600

    Brett and Matt pairing wrote calendar feed url to pass back all the private feed information

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
+  
   def index
+    client.authsub_token = params[:token] # extract the single-use token from the URL query params
+    session[:token] = client.auth_handler.upgrade()
+    client.authsub_token = session[:token] if session[:token]
     
+    @feed = client.get('http://docs.google.com/feeds/documents/private/full').to_xml
   end
   
   def home
