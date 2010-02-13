6f282c7443da334850eb6c33a119f90220588b0a
Jurand <jnogiec2@illinois.edu>
2010-02-13 17:51:38 -0600

    try to display calendars as feed elements

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,8 +8,8 @@ class HomeController < ApplicationController
     session[:token] = client.auth_handler.upgrade()
     client.authsub_token = session[:token] if session[:token]
     
-    @feeds = client.get("http://www.google.com/calendar/feeds/default/allcalendars/full").to_xml
-    
+    feed = client.get("http://www.google.com/calendar/feeds/default/allcalendars/full").to_xml
+    entry = feed.elements['entry']
          
   end
   
